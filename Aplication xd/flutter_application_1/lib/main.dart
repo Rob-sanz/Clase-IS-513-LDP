859f01f7d99f7fd088beefb9a49799fdb3ce0f1e
rob_sanz <robertosanchez@unah.hn>
2024-07-04 20:19:06 -0600

    Tarea 1.2 finalizada

---
+++ Aplication xd/flutter_application_1/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApps());
+}
+
+// 1. StatefullWidget (con estado)
+// 2. StatelessWidget (sin estado)
+
+class MyApp extends StatefulWidget {
+  @override
+  State createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int contador = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    final estilo = TextStyle(fontSize: 40, color: Colors.indigo);
+
+    return MaterialApp(
+      title: 'Mi primer app',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          // centerTitle: false,
+          backgroundColor: Colors.indigo[800],
+          // backgroundColor: Color(0xff009688),
+          title: Text(
+            'Mi primer app',
+            style: TextStyle(color: Colors.indigo[100]),
+          ),
+        ),
+        drawer: Drawer(),
+        body: Center(
+          child: Container(
+            // color: Colors.red,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Hizo tantas veces clic',
+                  style: estilo.copyWith(
+                      fontSize: contador.toDouble(),
+                      fontWeight: FontWeight.w300),
+                ),
+                Text(
+                  '$contador clics',
+                  style: estilo,
+                ),
+              ],
+            ),
+          ),
+        ),
+        // floatingActionButtonLocation:
+        //     FloatingActionButtonLocation.miniCenterTop,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // if (contador > 14) return;
+
+            contador++;
+
+            // notifica al widget que sus propiedades cambiaron
+            setState(() {});
+          },
+          child: Icon(
+            Icons.plus_one,
+            color: Colors.pink[800],
+          ),
+        ),
+      ),
+    );
+  }
+}
